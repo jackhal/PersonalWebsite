b8bacf2d741a2276533f3f593d0bd08a1a874001
jackhal <jackhale@gmail.com>
2023-07-14 15:30:08 -0700

    experience page layout changes

--- frontend/src/Pages/ExperiencePage.js
+++ frontend/src/Pages/ExperiencePage.js
@@ -23,25 +23,17 @@ export default function ExperiencePage() {
                             Undergraduate Research Assistant
                         </Typography>
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none', paddingLeft: '17.5%', paddingRight: '17.5%' }}>
-                            ● Wrote SAR for Microservice Systems Using GraalVM Native Image which was submitted to ASE 2023
-                        </Typography>
+                    <div className="container">
+                            <p className="text">● Created language-agnostic microservice intermediate representations with a team at Oracle by using GraalVM, Graal, Native Image, and Graal-Prophet</p>
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none', paddingLeft: '17.5%', paddingRight: '17.5%' }}>
-                            ● Developed language-agnostic microservice intermediate representations for an Oracle-backed project using GraalVM, Graal, Native Image, and Graal-Prophet
-                        </Typography>
+                    <div className="container">
+                            <p className="text">● Implemented static bytecode analysis to allow for anti-pattern detection and cyclic dependency discovery within microservice intermediate representations</p>
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none', paddingLeft: '17.5%', paddingRight: '17.5%' }}>
-                            ● Implemented static bytecode analysis of the intermediate representations to allow for anti-pattern detection and cyclic dependency discovery within microservice intermediate representations
-                        </Typography>
+                    <div className="container">
+                            <p className="text">● Developed a visualization tool to effectively showcase service dependency graphs and system context insights derived from the language-agnostic microservice intermediate representations</p>
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none', paddingLeft: '17.5%', paddingRight: '17.5%' }}>
-                            ● Developed a visualization tool to showcase service dependency graphs and system context insights derived from the language-agnostic microservice intermediate representations effectively
-                        </Typography>
+                    <div className="container">
+                            <p className="text">● Wrote SAR for Microservice Systems Using GraalVM Native Image which was submitted to ASE 2023</p>
                     </div>
                     <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'left' }}>
                         <Typography variant="body5" align="left" style={{ color: '#D0BDF4', textTransform: 'none', paddingLeft: '15%' }}>
@@ -56,20 +48,14 @@ export default function ExperiencePage() {
                             Software Engineering Intern
                         </Typography>
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none', paddingLeft: '17.5%', paddingRight: '17.5%' }}>
-                            • Processed flows including UX interactions to identify bottlenecks and further optimize the product
-                        </Typography>
+                    <div className="container">
+                        <p className="text">● Processed flows including UX interactions to identify bottlenecks and further optimize the product</p>
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none', paddingLeft: '17.5%', paddingRight: '17.5%' }}>
-                            • Presented analytics and potential solutions to management in a clear digestible manner
-                        </Typography>
+                    <div className="container">
+                        <p className="text">● Presented analytics and potential solutions to management in a clear digestible manner</p>
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none', paddingLeft: '17.5%', paddingRight: '17.5%' }}>
-                            • Focused on ease of use, clarity, and a streamlined experience for applicants to increase yield
-                        </Typography>
+                    <div className="container">
+                        <p className="text">● Focused on ease of use, clarity, and a streamlined experience for applicants to increase yield</p>
                     </div>
             </Box>
         </div>
