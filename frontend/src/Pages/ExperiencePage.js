68cf431370456b4cb5fb017140d373f39944c0ed
jackhal <jackhale@gmail.com>
2023-07-14 16:55:29 -0700

    Simplification to Experience Page styles

--- frontend/src/Pages/ExperiencePage.js
+++ frontend/src/Pages/ExperiencePage.js
@@ -10,53 +10,41 @@ export default function ExperiencePage() {
                 <Typography variant="h2" align="center" style={{ color: '#D0BDF4', textTransform: 'none' }}>
                     Experience
                 </Typography>
-                    <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'left' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#D0BDF4', textTransform: 'none', paddingLeft: '15%' }}>
-                            Baylor University,{'\u00A0'}
-                        </Typography>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none'}}>
-                            Waco, TX
-                        </Typography>
-                    </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'left' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#D0BDF4', textTransform: 'none', paddingLeft: '15%' }}>
-                            Undergraduate Research Assistant
-                        </Typography>
-                    </div>
-                    <div className="container">
-                            <p className="text">● Created language-agnostic microservice intermediate representations with a team at Oracle by using GraalVM, Graal, Native Image, and Graal-Prophet</p>
-                    </div>
-                    <div className="container">
-                            <p className="text">● Implemented static bytecode analysis to allow for anti-pattern detection and cyclic dependency discovery within microservice intermediate representations</p>
-                    </div>
-                    <div className="container">
-                            <p className="text">● Developed a visualization tool to effectively showcase service dependency graphs and system context insights derived from the language-agnostic microservice intermediate representations</p>
-                    </div>
-                    <div className="container">
-                            <p className="text">● Wrote SAR for Microservice Systems Using GraalVM Native Image which was submitted to ASE 2023</p>
-                    </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'left' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#D0BDF4', textTransform: 'none', paddingLeft: '15%' }}>
-                            Omni Interactions,{'\u00A0'}
-                        </Typography>
-                        <Typography variant="body5" align="left" style={{ color: '#E5EAF5', textTransform: 'none'}}>
-                            Denver, CO
-                        </Typography>
-                    </div>
-                    <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'left' }}>
-                        <Typography variant="body5" align="left" style={{ color: '#D0BDF4', textTransform: 'none', paddingLeft: '15%' }}>
-                            Software Engineering Intern
-                        </Typography>
-                    </div>
-                    <div className="container">
-                        <p className="text">● Processed flows including UX interactions to identify bottlenecks and further optimize the product</p>
-                    </div>
-                    <div className="container">
-                        <p className="text">● Presented analytics and potential solutions to management in a clear digestible manner</p>
-                    </div>
-                    <div className="container">
-                        <p className="text">● Focused on ease of use, clarity, and a streamlined experience for applicants to increase yield</p>
-                    </div>
+                <div className="container">
+                    <p className="company">Baylor University,{'\u00A0'}</p>
+                    <p className="location">Waco, TX</p>
+                </div>
+                <div className="container">
+                        <p className="jobTitle">Undergraduate Research Assistant</p>
+                </div>
+                <div className="container">
+                        <p className="text">● Created language-agnostic microservice intermediate representations with a team at Oracle by using GraalVM, Graal, Native Image, and Graal-Prophet</p>
+                </div>
+                <div className="container">
+                        <p className="text">● Implemented static bytecode analysis to allow for anti-pattern detection and cyclic dependency discovery within microservice intermediate representations</p>
+                </div>
+                <div className="container">
+                        <p className="text">● Developed a visualization tool to effectively showcase service dependency graphs and system context insights derived from the language-agnostic microservice intermediate representations</p>
+                </div>
+                <div className="container">
+                        <p className="text">● Wrote SAR for Microservice Systems Using GraalVM Native Image which was submitted to ASE 2023</p>
+                </div>
+                <div className="container">
+                    <p className="company">Omni Interactions,{'\u00A0'}</p>
+                    <p className="location">Denver, CO</p>
+                </div>
+                <div className="container">
+                        <p className="jobTitle">Software Engineering Intern</p>
+                </div>
+                <div className="container">
+                    <p className="text">● Processed flows including UX interactions to identify bottlenecks and further optimize the product</p>
+                </div>
+                <div className="container">
+                    <p className="text">● Presented analytics and potential solutions to management in a clear digestible manner</p>
+                </div>
+                <div className="container">
+                    <p className="text">● Focused on ease of use, clarity, and a streamlined experience for applicants to increase yield</p>
+                </div>
             </Box>
         </div>
     );
