9e17f5726e22b1e3c71aa86b49a4210ab476c72b
jackhal <jackhale@gmail.com>
2023-08-07 16:09:28 -0700

    Added About Me content, still needs styling

--- frontend/src/Pages/AboutPage.js
+++ frontend/src/Pages/AboutPage.js
@@ -23,6 +23,46 @@ export default function AboutPage() {
             <Typography variant="h2" align="center" style={{ color: '#D0BDF4', textTransform: 'none' }}>
                 About Me
             </Typography>
+            <p>Career Profile</p>
+            <p> Software engineerand accomplished leader, experienced in solving complex problems collaboratively. 
+                Strong foundation in algorithms, data structures, and software design with a track record of 
+                consistently meeting challenging project deadlines in fast-paced, agile environments. Committed to 
+                delivering excellent results, while working to create an inclusive, growth-oriented atmosphere.
+            </p>
+            <div>
+                <div>
+                    <p>Language Fluency:</p>
+                    <p>JavaScript, HTML/CSS, Java, Python, C, C++, Bash</p>
+                </div>
+                <div>
+                    <p>Other Technologies:</p>
+                    <p>Git, React, REST, Spring</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>Databases:</p>
+                    <p>MySQL, MariaDB</p>
+                </div>
+                <div>
+                    <p>Operating Systems:</p>
+                    <p>Linux, macOS, Windows</p>
+                </div>
+            </div>
+            <p>Education</p>
+            <div>
+                <div>
+                    <p>Baylor University,</p>
+                    <p>Waco, TX - Bachelor of Science in Computer Science</p>
+                </div>
+                <p>August 2019 - May 2023</p>
+            </div>
+            <p>• Experience leading multiple development teams in academia using agile methodologies</p>
+            <p>• Relevant Coursework: Data Structures and Algorithms, Database Design, Data Communications, 
+                Operating Systems, Computer Architecture, and Cybersecurity
+            </p>
+            <p>Additional Information</p>
+            <p>● Enjoys community service, story-based video games, Labradoodles, traveling, and hiking</p>
         </div>
     );
 }
