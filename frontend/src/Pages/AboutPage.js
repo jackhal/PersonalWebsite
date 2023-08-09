4f3546648d5f12d4c0b0d4c47b11452c4b54882f
jackhal <jackhale@gmail.com>
2023-08-08 17:08:00 -0700

    Formatted About page, may need more formatting for different screen sizes

--- frontend/src/Pages/AboutPage.js
+++ frontend/src/Pages/AboutPage.js
@@ -20,9 +20,7 @@ export default function AboutPage() {
 
     return (
         <div style={ aboutPageContent }>
-            <Typography variant="h2" align="center" style={{ color: '#D0BDF4', textTransform: 'none' }}>
-                About Me
-            </Typography>
+            <p class="about-header">About Me</p>
             <div class="about-content">
                 <p class="section-heading">Career Profile</p>
                 <p class="pure-paragraph"> Software engineerand accomplished leader, experienced in solving complex 
@@ -34,39 +32,39 @@ export default function AboutPage() {
                 <div class="tech-section">
                     <div class="tech-line">
                         <div class="tech-left">
-                            <p class="tech-type">Language Fluency:</p>
+                            <p class="tech-type">Language Fluency:{'\u00A0'}</p>
                             <p class="tech-list">JavaScript, HTML/CSS, Java, Python, C, C++, Bash</p>
                         </div>
                         <div class="tech-right">
-                            <p class="tech-type">Other Technologies:</p>
+                            <p class="tech-type">Other Technologies:{'\u00A0'}</p>
                             <p class="tech-list">Git, React, REST, Spring</p>
                         </div>
                     </div>
                     <div class="tech-line">
                         <div class="tech-left">
-                            <p class="tech-type">Databases:</p>
+                            <p class="tech-type">Databases:{'\u00A0'}</p>
                             <p class="tech-list">MySQL, MariaDB</p>
                         </div>
                         <div class="tech-right">
-                            <p class="tech-type">Operating Systems:</p>
+                            <p class="tech-type">Operating Systems:{'\u00A0'}</p>
                             <p class="tech-list">Linux, macOS, Windows</p>
                         </div>
                     </div>
                 </div>
                 <p class="section-heading">Education</p>
-                <div>
-                    <div>
-                        <p class="school-name">Baylor University,</p>
+                <div class="school-group">
+                    <div class="school-info">
+                        <p class="school-name">Baylor University,{'\u00A0'}</p>
                         <p class="school-location">Waco, TX - Bachelor of Science in Computer Science</p>
                     </div>
                     <p class="date">August 2019 - May 2023</p>
                 </div>
-                <p class="pure-paragraph">• Experience leading multiple development teams in academia using agile methodologies</p>
-                <p class="pure-paragraph">• Relevant Coursework: Data Structures and Algorithms, Database Design, Data Communications, 
+                <p class="indent-paragraph">● Experience leading multiple development teams in academia using agile methodologies</p>
+                <p class="indent-paragraph">● Relevant Coursework: Data Structures and Algorithms, Database Design, Data Communications, 
                     Operating Systems, Computer Architecture, and Cybersecurity
                 </p>
                 <p class="section-heading">Additional Information</p>
-                <p class="pure-paragraph">● Enjoys community service, story-based video games, Labradoodles, traveling, and hiking</p>
+                <p class="indent-paragraph">● Enjoys community service, story-based video games, Labradoodles, traveling, and hiking</p>
             </div>
         </div>
     );
