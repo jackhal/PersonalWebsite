db22ba948fd5607350693e1d5cbe66cf8e6efee2
jackhal <jackhale@gmail.com>
2023-08-10 17:10:16 -0700

    PDF can be viewed using embed HTML, but want to use PDF.js instead

--- frontend/src/Pages/ResumePage.js
+++ frontend/src/Pages/ResumePage.js
@@ -1,5 +1,5 @@
+import React, { useContext, useEffect, useRef } from 'react';
 import Typography from '@mui/material/Typography';
-import React, { useContext } from 'react';
 import Button from '@mui/material/Button';
 import { HeaderContext } from "../Contexts/HeaderContext.js";
 import { FooterContext } from "../Contexts/FooterContext.js";
@@ -9,24 +9,24 @@ export default function ResumePage() {
     
     const { headerHeight } = useContext(HeaderContext);
     const { footerHeight } = useContext(FooterContext);
-    
+
+
     const resumePageContent = {
         overflowY: 'auto',
         position: 'fixed',
         top: headerHeight,
         bottom: footerHeight,
         width: '100%',
-        height: `calc(100svh - ${headerHeight}px - ${footerHeight}px - 10px)`,
+        height: `calc(100vh - ${headerHeight}px - ${footerHeight}px - 10px)`,
     };
 
     return (
-        <div style={ resumePageContent }>
-            <Typography variant="h2" align="center" style={{ color: '#D0BDF4', textTransform: 'none' }}>
-                My Resume
-            </Typography>
+        <div style={resumePageContent}>
+            <p class="resume-header">My Resume</p>
             <a href="/downloads/JackHale_Resume.pdf" download>
                 <button class="button-85" role="button">Download</button>
             </a>
+            <embed src={process.env.PUBLIC_URL + '/downloads/JackHale_Resume.pdf'} type="application/pdf" width="100%" height="80%" />
         </div>
     );
 }
